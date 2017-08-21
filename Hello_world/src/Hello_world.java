3ee2d33414941761e1c67919422353c3f155065e
SauceBoss1 <dtvsworld@gmail.com>
2017-08-20 21:55:36 -0700

    aug162017 Hello World demonstration
    Making this application helped bring out the old me. This "old me" was when I was experiencing python for the first time.

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,17 @@
+/*
+ * program states hello world
+ * 
+ * @author derfelterciano
+ * @version 01
+ * 
+ * 
+ */
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
