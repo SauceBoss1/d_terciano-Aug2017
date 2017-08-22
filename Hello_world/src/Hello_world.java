476585653031f9c489015470b851f90fd1c8c070
SauceBoss1 <dtvsworld@gmail.com>
2017-08-22 15:08:52 -0700

    Hello World
      I feel like my "old self" when doing this. THe old self was when I
    learned python again.

--- Hello_world/src/Hello_world.java
+++ Hello_world/src/Hello_world.java
@@ -10,7 +10,8 @@ public class Hello_world {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi theres World");
+		System.out.println("Hi theres World!");
+		System.out.print("hello");
 
 	}
 
