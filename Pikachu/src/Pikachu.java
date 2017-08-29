f636d93404de9d7469d2e1892d7ce70e3e21b4dd
SauceBoss1 <dtvsworld@gmail.com>
2017-08-29 15:33:12 -0700

    this is a test 	this is something im trying

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;  
+public class Pikachu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your rollno");  
+		   int rollno=sc.nextInt();  
+		   System.out.println("Enter your name");  
+		   String name=sc.next();  
+		   System.out.println("Enter your fee");  
+		   double fee=sc.nextDouble();  
+		   System.out.println("Rollno:"+rollno+" name:"+name+" fee:"+fee);  
+		   sc.close();  
+	}
+
+}
